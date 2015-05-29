8dc5ca64975ae66b888a2122259948ed5fb7a5fe
Christopher Vezzuto <christopher.vezzuto@ticketmaster.com>
2015-05-29 11:58:10 -0700

    starting game logic

--- WebContent/index.js
+++ WebContent/index.js
@@ -4,9 +4,15 @@ var board = [
              ["", "", ""],
              ];
 
+var turn = 1;
+
 function canvasClicked(row, column){
-	board[row][column] = "X";
-	htmlDraw();
+	if(turn % 2 === 1){
+		board[row][column] = "X";
+		htmlDraw();
+		turn++; 
+		computerTurn();
+	}
 };
 
 function htmlDraw(){
@@ -24,7 +30,24 @@ function htmlDraw(){
 	            cxt.stroke();
 	            cxt.closePath();
 			}
+			if(space === "O"){
+				cxt.beginPath();
+				cxt.arc(25,25,20,0,Math.PI*2,true);
+				cxt.stroke();
+				cxt.closePath();
+			}
 
 		});
 	});
 };
+
+function computerTurn(){
+	if(turn % 2 === 0){
+		//first check for offensive (do you have 2 in a row with an open spot)...then win
+		
+		//then check defensive (do they have 2 in a row with an open spot)...then block
+		//if no unblocked pairs, block the X with the most blank Axis using logical priority
+	}
+};
+
+
--- WebContent/tests.js
+++ WebContent/tests.js
@@ -17,7 +17,32 @@ test("space should register when clicked", function () {
 	ok(board[0][0] === "X");
 });
 
+module('game');
 
+test("turn should begin at 1 and advance when user clicks a square", function () {
+	canvasClicked(1,0);
+	ok(turn === 2);
+});
+
+test("2 Xs on 1 axis should be blocked by O", function () {
+	board = [
+             ["X", "", "X"],
+             ["", "O", ""],
+             ["", "", ""],
+             ];
+	computerTurn();
+	ok(board[0][1] === "O");
+});
+
+test("2 Os on 1 axis with an open space should have computer win", function (){
+	board = [
+             ["X", "O", "X"],
+             ["X", "O", ""],
+             ["", "", ""],
+             ];
+	computerTurn();
+	ok(board[2][1] === "O");
+});
 
 //mocks
 function htmlDraw() {};
