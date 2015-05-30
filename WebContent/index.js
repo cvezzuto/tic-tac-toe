470061f9c98b0136defa2e7474130c08c3855e9b
Christopher Vezzuto <christopher.vezzuto@ticketmaster.com>
2015-05-29 17:34:01 -0700

    basic logic (needs 1 more rule)

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,7 +1,7 @@
 var board = [
              ["", "", ""],
              ["", "", ""],
-             ["", "", ""],
+             ["", "", ""]
              ];
 
 var turn = 1;
@@ -42,11 +42,167 @@ function htmlDraw(){
 };
 
 function computerTurn(){
+	//if first move is in a corner, then you need to place a block in the opposite corner
+	
 	if(turn % 2 === 0){
-		//first check for offensive (do you have 2 in a row with an open spot)...then win
-		
-		//then check defensive (do they have 2 in a row with an open spot)...then block
-		//if no unblocked pairs, block the X with the most blank Axis using logical priority
+		//iterate through each row on the board
+		_.forEach(board, function(row, rowIndex) {
+			var rowCount = _.countBy(row);
+			//first check for offensive (do you have 2 in a row with an open spot)...then win
+			if(rowCount["O"] === 2 && rowCount[""] === 1 && turn % 2 === 0){
+				var openIndex = _.indexOf(row, "");
+				board[rowIndex][openIndex] = "O";
+				htmlDraw();
+				turn++;
+				
+			}
+			//then check defensive (do they have 2 in a row with an open spot)...then block
+			else if(rowCount["X"] === 2 && rowCount[""] === 1 && turn % 2 === 0){
+				var openIndex = _.indexOf(row, "");
+				board[rowIndex][openIndex] = "O";
+				htmlDraw();
+				turn++;
+				
+			}
+		});
+	}
+	if(turn % 2 === 0){
+		//iterate through each column on the board 
+		_.forEach([
+		          [board[0][0], board[1][0], board[2][0]],
+		          [board[0][1], board[1][1], board[2][1]],
+		          [board[0][2], board[1][2], board[2][2]]
+				], function(column, columnIndex){
+			var columnCount = _.countBy(column);
+			if(columnCount["O"] === 2 && columnCount[""] === 1 && turn % 2 === 0){
+				var openIndex = _.indexOf(column, "");
+				board[openIndex][columnIndex] = "O";
+				htmlDraw();
+				turn++;
+				
+			}
+			//then check defensive (do they have 2 in a row with an open spot)...then block
+			else if(columnCount["X"] === 2 && columnCount[""] === 1 && turn % 2 === 0){
+				var openIndex = _.indexOf(column, "");
+				board[openIndex][columnIndex] = "O";
+				htmlDraw();
+				turn++;
+				
+			}
+		});
+	}
+	if(turn % 2 === 0){
+		//iterate through each diagonal
+		_.forEach([
+			          [board[0][0], board[1][1], board[2][2]],
+			          [board[0][2], board[1][1], board[2][0]]
+					], function(diag, diagIndex){
+				var diagCount = _.countBy(diag);
+				if(diagCount["O"] === 2 && diagCount[""] === 1 && turn % 2 === 0){
+					var openIndex = _.indexOf(diag, "");
+					if(diagIndex === 0){
+						board[openIndex][openIndex] = "O";
+					}
+					if(diagIndex === 1){
+						if(openIndex === 0){
+							board[openIndex][2] = "O";
+						}
+						else if(openIndex === 1){
+							board[openIndex][openIndex]  = "O";
+						}
+						else if(openIndex === 2){
+							board[openIndex][0]  = "O";
+						}
+					}
+					htmlDraw();
+					turn++;
+					
+				}
+				//then check defensive (do they have 2 in a row with an open spot)...then block
+				else if(diagCount["X"] === 2 && diagCount[""] === 1 && turn % 2 === 0){
+					var openIndex = _.indexOf(diag, "");
+					if(diagIndex === 0){
+						board[openIndex][openIndex] = "O";
+					}
+					if(diagIndex === 1){
+						if(openIndex === 0){
+							board[openIndex][2]  = "O";
+						}
+						else if(openIndex === 1){
+							board[openIndex][openIndex]  = "O";
+						}
+						else if(openIndex === 2){
+							board[openIndex][0]  = "O";
+						}
+					}
+					htmlDraw();
+					turn++;
+					
+				}
+			});
+	}
+	if(turn % 2 === 0){
+	//if no unblocked pairs, block the X with the most blank Axis using logical priority
+		//first, check the middle
+		if(board[1][1] === ""){
+			board[1][1] = "O";
+			htmlDraw();
+			turn++;
+		}
+		//then the corners
+		else if(turn % 2 === 0){
+			_.forEach([board[0][0], board[0][2], board[2][0], board[2][2]], function(corner, cornerIndex){
+				if(corner === "" && turn % 2 === 0){
+					if(cornerIndex === 0){
+						board[0][0] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 1){
+						board[0][2] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 2){
+						board[2][0] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 3){
+						board[2][2] = "O";
+						htmlDraw();
+						turn++;
+					}
+				}
+			});
+		}
+		//then the edges
+		else {
+			_.forEach([board[0][1], board[1][0], board[1][2], board[2][1]], function(corner, cornerIndex){
+				if(corner === "" && turn % 2 === 0){
+					if(cornerIndex === 0){
+						board[0][1] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 1){
+						board[1][0] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 2){
+						board[1][2] = "O";
+						htmlDraw();
+						turn++;
+					}
+					if(cornerIndex === 3){
+						board[2][1] = "O";
+						htmlDraw();
+						turn++;
+					}
+				}
+			});
+		}
 	}
 };
 
--- WebContent/tests.js
+++ WebContent/tests.js
@@ -20,28 +20,113 @@ test("space should register when clicked", function () {
 module('game');
 
 test("turn should begin at 1 and advance when user clicks a square", function () {
+	turn = 1;
+	board = [
+             ["", "", ""],
+             ["", "", ""],
+             ["", "", ""]
+             ];
 	canvasClicked(1,0);
-	ok(turn === 2);
+	ok(turn === 3);
 });
 
-test("2 Xs on 1 axis should be blocked by O", function () {
+test("2 Xs on 1 row should be blocked by O", function () {
+	turn = 4;
 	board = [
              ["X", "", "X"],
              ["", "O", ""],
-             ["", "", ""],
+             ["", "", ""]
              ];
 	computerTurn();
 	ok(board[0][1] === "O");
 });
 
-test("2 Os on 1 axis with an open space should have computer win", function (){
+test("2 Os on 1 row with an open space should have computer win", function (){
+	turn = 4;
+	board = [
+             ["X", "", ""],
+             ["O", "O", ""],
+             ["X", "", ""]
+             ];
+	computerTurn();
+	ok(board[1][2] === "O");
+});
+
+test("2 Xs on 1 column should be blocked by O", function () {
+	turn = 4;
+	board = [
+             ["X", "", ""],
+             ["", "O", ""],
+             ["X", "", ""]
+             ];
+	computerTurn();
+	ok(board[1][0] === "O");
+});
+
+test("2 Os on 1 column with an open space should have computer win", function (){
+	turn = 4;
+	board = [
+             ["X", "", "X"],
+             ["", "O", ""],
+             ["", "O", ""]
+             ];
+	computerTurn();
+	ok(board[0][1] === "O");
+});
+
+test("2 Xs on 1 diagonal should be blocked by O", function () {
+	turn = 4;
+	board = [
+             ["X", "", ""],
+             ["", "", "O"],
+             ["", "", "X"]
+             ];
+	computerTurn();
+	ok(board[1][1] === "O");
+});
+
+test("2 Os on 1 diagonal with an open space should have computer win", function (){
+	turn = 6;
+	board = [
+             ["X", "", ""],
+             ["X", "O", ""],
+             ["O", "X", ""]
+             ];
+	computerTurn();
+	ok(board[0][2] === "O");
+});
+
+test("an open turn should prioritize the middle", function (){
+	turn = 2;
+	board = [
+             ["X", "", ""],
+             ["", "", ""],
+             ["", "", ""]
+             ];
+	computerTurn();
+	ok(board[1][1] === "O");
+});
+
+test("an open turn should prioritize the corners if middle is taken", function (){
+	turn = 4;
+	board = [
+             ["X", "", ""],
+             ["", "O", ""],
+             ["", "", "X"]
+             ];
+	computerTurn();
+	ok(board[0][2] === "O");
+});
+
+test("an open turn should prioritize the edges if corners and middle are taken", function (){
+	turn = 8;
 	board = [
              ["X", "O", "X"],
              ["X", "O", ""],
-             ["", "", ""],
+             ["O", "X", "X"]
              ];
 	computerTurn();
-	ok(board[2][1] === "O");
+	ok(board[1][2] === "O");
 });
 
 //mocks
