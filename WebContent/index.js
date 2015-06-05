607fabb88cb181122cb46c490cf3c00b4f41fde3
Christopher Vezzuto <christopher.vezzuto@ticketmaster.com>
2015-06-05 11:59:40 -0700

    BOARD NAMESPACE
    Create namspace for board with getters for rows,
    columns, diagnonals, etc.

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,14 +1,47 @@
-var board = [
+var BOARD = BOARD || {};
+
+BOARD.board = [
              ["", "", ""],
              ["", "", ""],
              ["", "", ""]
              ];
 
+BOARD.get = {
+	rows: function(){
+		return BOARD.board;
+	},
+	columns: function(){
+		return [[BOARD.board[0][0], BOARD.board[1][0], BOARD.board[2][0]],
+        [BOARD.board[0][1], BOARD.board[1][1], BOARD.board[2][1]],
+        [BOARD.board[0][2], BOARD.board[1][2], BOARD.board[2][2]]];
+	},
+	diagonals: function(){
+		return [[BOARD.board[0][0], BOARD.board[1][1], BOARD.board[2][2]],
+        [BOARD.board[0][2], BOARD.board[1][1], BOARD.board[2][0]]];
+	},
+	middle: function(){
+		return BOARD.board[1][1];
+	},
+	corners: function(){
+		return [BOARD.board[0][0], BOARD.board[0][2], BOARD.board[2][0], BOARD.board[2][2]];
+	},
+	cornerPairs: function(){
+		return [
+		          [BOARD.board[0][0], BOARD.board[2][2]],
+		          [BOARD.board[2][0], BOARD.board[0][2]]
+				];
+	},
+	edges: function(){
+		return [BOARD.board[0][1], BOARD.board[1][0], BOARD.board[1][2], BOARD.board[2][1]];
+	}
+}
+
+
 var turn = 1;
 
 function canvasClicked(row, column){
 	if(turn % 2 === 1){
-		board[row][column] = "X";
+		BOARD.board[row][column] = "X";
 		htmlDraw();
 		turn++; 
 		computerTurn();
@@ -17,7 +50,7 @@ function canvasClicked(row, column){
 
 function htmlDraw(){
 	
-	_.forEach(board, function(row, rowIndex) {
+	_.forEach(BOARD.board, function(row, rowIndex) {
 		_.forEach(row, function(space, columnIndex) {
 			c = document.getElementById("canvas_" + rowIndex + "_" + columnIndex);
 			cxt = c.getContext("2d");
@@ -45,13 +78,13 @@ function computerTurn(){
 
 	//OFFENSIVE
 	if(turn % 2 === 0){
-		//iterate through each row on the board
-		_.forEach(board, function(row, rowIndex) {
+		//iterate through each row on the BOARD.board
+		_.forEach(BOARD.get.rows(), function(row, rowIndex) {
 			var rowCount = _.countBy(row);
 			//first check for offensive (do you have 2 in a row with an open spot)...then win
 			if(rowCount["O"] === 2 && rowCount[""] === 1 && turn % 2 === 0){
 				var openIndex = _.indexOf(row, "");
-				board[rowIndex][openIndex] = "O";
+				BOARD.board[rowIndex][openIndex] = "O";
 				htmlDraw();
 				turn++;
 				
@@ -59,16 +92,12 @@ function computerTurn(){
 		});
 	}
 	if(turn % 2 === 0){
-		//iterate through each column on the board 
-		_.forEach([
-		          [board[0][0], board[1][0], board[2][0]],
-		          [board[0][1], board[1][1], board[2][1]],
-		          [board[0][2], board[1][2], board[2][2]]
-				], function(column, columnIndex){
+		//iterate through each column on the BOARD.board 
+		_.forEach(BOARD.get.columns(), function(column, columnIndex){
 			var columnCount = _.countBy(column);
 			if(columnCount["O"] === 2 && columnCount[""] === 1 && turn % 2 === 0){
 				var openIndex = _.indexOf(column, "");
-				board[openIndex][columnIndex] = "O";
+				BOARD.board[openIndex][columnIndex] = "O";
 				htmlDraw();
 				turn++;
 				
@@ -77,25 +106,22 @@ function computerTurn(){
 	}
 	if(turn % 2 === 0){
 		//iterate through each diagonal
-		_.forEach([
-			          [board[0][0], board[1][1], board[2][2]],
-			          [board[0][2], board[1][1], board[2][0]]
-					], function(diag, diagIndex){
+		_.forEach(BOARD.get.diagonals(), function(diag, diagIndex){
 				var diagCount = _.countBy(diag);
 				if(diagCount["O"] === 2 && diagCount[""] === 1 && turn % 2 === 0){
 					var openIndex = _.indexOf(diag, "");
 					if(diagIndex === 0){
-						board[openIndex][openIndex] = "O";
+						BOARD.board[openIndex][openIndex] = "O";
 					}
 					if(diagIndex === 1){
 						if(openIndex === 0){
-							board[openIndex][2] = "O";
+							BOARD.board[openIndex][2] = "O";
 						}
 						else if(openIndex === 1){
-							board[openIndex][openIndex]  = "O";
+							BOARD.board[openIndex][openIndex]  = "O";
 						}
 						else if(openIndex === 2){
-							board[openIndex][0]  = "O";
+							BOARD.board[openIndex][0]  = "O";
 						}
 					}
 					htmlDraw();
@@ -108,15 +134,15 @@ function computerTurn(){
 	
 	//DEFENSIVE
 	if(turn % 2 === 0){
-		//iterate through each row on the board
-		_.forEach(board, function(row, rowIndex) {
+		//iterate through each row on the BOARD.board
+		_.forEach(BOARD.get.rows(), function(row, rowIndex) {
 			var rowCount = _.countBy(row);
 			//first check for offensive (do you have 2 in a row with an open spot)...then win
 
 			//then check defensive (do they have 2 in a row with an open spot)...then block
 			if(rowCount["X"] === 2 && rowCount[""] === 1 && turn % 2 === 0){
 				var openIndex = _.indexOf(row, "");
-				board[rowIndex][openIndex] = "O";
+				BOARD.board[rowIndex][openIndex] = "O";
 				htmlDraw();
 				turn++;
 				
@@ -124,18 +150,14 @@ function computerTurn(){
 		});
 	}
 	if(turn % 2 === 0){
-		//iterate through each column on the board 
-		_.forEach([
-		          [board[0][0], board[1][0], board[2][0]],
-		          [board[0][1], board[1][1], board[2][1]],
-		          [board[0][2], board[1][2], board[2][2]]
-				], function(column, columnIndex){
+		//iterate through each column on the BOARD.board 
+		_.forEach(BOARD.get.columns(), function(column, columnIndex){
 			var columnCount = _.countBy(column);
 
 			//then check defensive (do they have 2 in a row with an open spot)...then block
 			if(columnCount["X"] === 2 && columnCount[""] === 1 && turn % 2 === 0){
 				var openIndex = _.indexOf(column, "");
-				board[openIndex][columnIndex] = "O";
+				BOARD.board[openIndex][columnIndex] = "O";
 				htmlDraw();
 				turn++;
 				
@@ -144,27 +166,24 @@ function computerTurn(){
 	}
 	if(turn % 2 === 0){
 		//iterate through each diagonal
-		_.forEach([
-			          [board[0][0], board[1][1], board[2][2]],
-			          [board[0][2], board[1][1], board[2][0]]
-					], function(diag, diagIndex){
+		_.forEach(BOARD.get.diagonals(), function(diag, diagIndex){
 				var diagCount = _.countBy(diag);
 
 				//then check defensive (do they have 2 in a row with an open spot)...then block
 				if(diagCount["X"] === 2 && diagCount[""] === 1 && turn % 2 === 0){
 					var openIndex = _.indexOf(diag, "");
 					if(diagIndex === 0){
-						board[openIndex][openIndex] = "O";
+						BOARD.board[openIndex][openIndex] = "O";
 					}
 					if(diagIndex === 1){
 						if(openIndex === 0){
-							board[openIndex][2]  = "O";
+							BOARD.board[openIndex][2]  = "O";
 						}
 						else if(openIndex === 1){
-							board[openIndex][openIndex]  = "O";
+							BOARD.board[openIndex][openIndex]  = "O";
 						}
 						else if(openIndex === 2){
-							board[openIndex][0]  = "O";
+							BOARD.board[openIndex][0]  = "O";
 						}
 					}
 					htmlDraw();
@@ -179,43 +198,36 @@ function computerTurn(){
 	if(turn % 2 === 0){
 	//if no unblocked pairs, block the X with the most blank Axis using logical priority
 		//first, check the middle
-		if(board[1][1] === ""){
-			board[1][1] = "O";
+		if(BOARD.get.middle() === ""){
+			BOARD.board[1][1] = "O";
 			htmlDraw();
 			turn++;
 		}
 		//then check to see if the player has 2 opposite corners...if they do, create 2 in a row
 		if(turn % 2 === 0){
-			//iterate through each corner combo on the board 
-			_.forEach([
-			          [board[0][0], board[2][2]],
-			          [board[2][0], board[0][2]]
-					], function(corners, cornersIndex){
+			//iterate through each corner combo on the BOARD.board 
+			_.forEach(BOARD.get.cornerPairs(), function(corners, cornersIndex){
 				var cornerCount = _.countBy(corners);
 				if(cornerCount["X"] === 2){
 					//create 2 in a row using one of the sides
-					_.forEach(board, function(row, rowIndex) {
+					_.forEach(BOARD.get.rows(), function(row, rowIndex) {
 						var rowCount = _.countBy(row);
 						//if you have 1 with an open space
 						if(rowCount["O"] === 1 && rowCount[""] === 2 && turn % 2 === 0){
 							var openIndex = _.indexOf(row, "");
-							board[rowIndex][openIndex] = "O";
+							BOARD.board[rowIndex][openIndex] = "O";
 							htmlDraw();
 							turn++;
 						}
 					});
 					if(turn % 2 === 0){
 						//if you don't find any, go ahead and check column ways
-						//iterate through each column on the board 
-						_.forEach([
-						          [board[0][0], board[1][0], board[2][0]],
-						          [board[0][1], board[1][1], board[2][1]],
-						          [board[0][2], board[1][2], board[2][2]]
-								], function(column, columnIndex){
+						//iterate through each column on the BOARD.board 
+						_.forEach(BOARD.get.columns(), function(column, columnIndex){
 							var columnCount = _.countBy(column);
 							if(columnCount["O"] === 1 && columnCount[""] === 2 && turn % 2 === 0){
 								var openIndex = _.indexOf(column, "");
-								board[openIndex][columnIndex] = "O";
+								BOARD.board[openIndex][columnIndex] = "O";
 								htmlDraw();
 								turn++;
 								
@@ -227,47 +239,47 @@ function computerTurn(){
 		}
 		//then the corners (prioritizing opposite corners)
 		if(turn % 2 === 0){
-			_.forEach([board[0][0], board[0][2], board[2][0], board[2][2]], function(corner, cornerIndex){
+			_.forEach(BOARD.get.corners(), function(corner, cornerIndex){
 				if(corner === "X" && turn % 2 === 0){
-					if(cornerIndex === 0 && board[2][2] === ""){
-						board[2][2] = "O";
+					if(cornerIndex === 0 && BOARD.board[2][2] === ""){
+						BOARD.board[2][2] = "O";
 						htmlDraw();
 						turn++;
 					}
-					if(cornerIndex === 1 && board[2][0] === ""){
-						board[2][0] = "O";
+					if(cornerIndex === 1 && BOARD.board[2][0] === ""){
+						BOARD.board[2][0] = "O";
 						htmlDraw();
 						turn++;
 					}
-					if(cornerIndex === 2 && board[0][2] === ""){
-						board[0][2] = "O";
+					if(cornerIndex === 2 && BOARD.board[0][2] === ""){
+						BOARD.board[0][2] = "O";
 						htmlDraw();
 						turn++;
 					}
-					if(cornerIndex === 3 && board[0][0] === ""){
-						board[0][0] = "O";
+					if(cornerIndex === 3 && BOARD.board[0][0] === ""){
+						BOARD.board[0][0] = "O";
 						htmlDraw();
 						turn++;
 					}
 				}
 				else if(corner === "" && turn % 2 === 0){
 					if(cornerIndex === 0){
-						board[0][0] = "O";
+						BOARD.board[0][0] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 1){
-						board[0][2] = "O";
+						BOARD.board[0][2] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 2){
-						board[2][0] = "O";
+						BOARD.board[2][0] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 3){
-						board[2][2] = "O";
+						BOARD.board[2][2] = "O";
 						htmlDraw();
 						turn++;
 					}
@@ -276,25 +288,25 @@ function computerTurn(){
 		}
 		//then the edges
 		if(turn % 2 === 0) {
-			_.forEach([board[0][1], board[1][0], board[1][2], board[2][1]], function(corner, cornerIndex){
+			_.forEach(BOARD.get.edges(), function(corner, cornerIndex){
 				if(corner === "" && turn % 2 === 0){
 					if(cornerIndex === 0){
-						board[0][1] = "O";
+						BOARD.board[0][1] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 1){
-						board[1][0] = "O";
+						BOARD.board[1][0] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 2){
-						board[1][2] = "O";
+						BOARD.board[1][2] = "O";
 						htmlDraw();
 						turn++;
 					}
 					if(cornerIndex === 3){
-						board[2][1] = "O";
+						BOARD.board[2][1] = "O";
 						htmlDraw();
 						turn++;
 					}
--- WebContent/tests.js
+++ WebContent/tests.js
@@ -1,11 +1,11 @@
-module('board');
+module('BOARD.board');
 
-test("board has three rows", function() { 
- ok(board.length === 3); 
+test("BOARD.board has three rows", function() { 
+ ok(BOARD.board.length === 3); 
 });
 
-test("board has 3 columns in each row", function () {
-  ok(board[0].length === 3);
+test("BOARD.board has 3 columns in each row", function () {
+  ok(BOARD.board[0].length === 3);
 });
 
 
@@ -14,14 +14,14 @@ module('UX');
 test("space should register when clicked", function () {
 	
 	canvasClicked(0, 0);  
-	ok(board[0][0] === "X");
+	ok(BOARD.board[0][0] === "X");
 });
 
 module('game');
 
 test("turn should begin at 1 and advance when user clicks a square", function () {
 	turn = 1;
-	board = [
+	BOARD.board = [
              ["", "", ""],
              ["", "", ""],
              ["", "", ""]
@@ -32,101 +32,101 @@ test("turn should begin at 1 and advance when user clicks a square", function ()
 
 test("2 Xs on 1 row should be blocked by O", function () {
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", "X"],
              ["", "O", ""],
              ["", "", ""]
              ];
 	computerTurn();
-	ok(board[0][1] === "O");
+	ok(BOARD.board[0][1] === "O");
 });
 
 test("2 Os on 1 row with an open space should have computer win", function (){
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["O", "O", ""],
              ["X", "", ""]
              ];
 	computerTurn();
-	ok(board[1][2] === "O");
+	ok(BOARD.board[1][2] === "O");
 });
 
 test("2 Xs on 1 column should be blocked by O", function () {
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["", "O", ""],
              ["X", "", ""]
              ];
 	computerTurn();
-	ok(board[1][0] === "O");
+	ok(BOARD.board[1][0] === "O");
 });
 
 test("2 Os on 1 column with an open space should have computer win", function (){
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", "X"],
              ["", "O", ""],
              ["", "O", ""]
              ];
 	computerTurn();
-	ok(board[0][1] === "O");
+	ok(BOARD.board[0][1] === "O");
 });
 
 test("2 Xs on 1 diagonal should be blocked by O", function () {
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["", "", "O"],
              ["", "", "X"]
              ];
 	computerTurn();
-	ok(board[1][1] === "O");
+	ok(BOARD.board[1][1] === "O");
 });
 
 test("2 Os on 1 diagonal with an open space should have computer win", function (){
 	turn = 6;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["X", "O", ""],
              ["O", "X", ""]
              ];
 	computerTurn();
-	ok(board[0][2] === "O");
+	ok(BOARD.board[0][2] === "O");
 });
 
 test("an open turn should prioritize the middle", function (){
 	turn = 2;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["", "", ""],
              ["", "", ""]
              ];
 	computerTurn();
-	ok(board[1][1] === "O");
+	ok(BOARD.board[1][1] === "O");
 });
 
 test("an open turn should prioritize the corners if middle is taken unless a fork is created", function (){
 	turn = 4;
-	board = [
+	BOARD.board = [
              ["X", "", ""],
              ["", "O", ""],
              ["", "", "X"]
              ];
 	computerTurn();
-	ok(board[1][0] === "O");
+	ok(BOARD.board[1][0] === "O");
 });
 
 test("an open turn should prioritize the edges if corners and middle are taken", function (){
 	turn = 8;
-	board = [
+	BOARD.board = [
              ["X", "O", "X"],
              ["X", "O", ""],
              ["O", "X", "X"]
              ];
 	computerTurn();
-	ok(board[1][2] === "O");
+	ok(BOARD.board[1][2] === "O");
 });
 
 //mocks
